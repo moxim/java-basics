b9f4b18f972396d868ac68b2a6ec6be553d16318
Michael Wiemder <Michael.Wiedmer@pdg-software.com>
2021-12-08 08:29:15 +0100

    show effect of static and main

--- src/main/java/ch/finnova/java/schulung/people/Person.java
+++ src/main/java/ch/finnova/java/schulung/people/Person.java
@@ -1,4 +1,32 @@
 package ch.finnova.java.schulung.people;
 
 public class Person {
+    private static String name;
+
+    public void setName(String newName) {
+        name = newName; // referencing a static variable from within a non-static method is pretty darn dangerous
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    public void sayHello() {
+        System.out.println(name + " says: Hello");
+    }
+
+    public void sayHelloTo(Person eve) {
+        System.out.println(name + " says: Hello " + eve.getName());
+    }
+
+     public static void main(String[] args) {
+        Person adam = new Person();
+        adam.setName("Adam");
+
+        Person eve = new Person();
+        eve.setName("Eve");
+
+        adam.sayHello();
+        adam.sayHelloTo(eve);
+    }
 }
