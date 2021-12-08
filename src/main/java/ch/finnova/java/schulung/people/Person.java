6eb9e2798e2c601865c451574534afddd02184e8
Michael Wiemder <Michael.Wiedmer@pdg-software.com>
2021-12-08 20:01:09 +0100

    use final variable

--- src/main/java/ch/finnova/java/schulung/people/Person.java
+++ src/main/java/ch/finnova/java/schulung/people/Person.java
@@ -1,4 +1,13 @@
 package ch.finnova.java.schulung.people;
 
 public class Person {
+    private final String name;
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public Person(String newName) {
+        name = newName;
+    }
 }
