a9888cb841afa3414dedbd75867597a04b3b552b
Michael Wiemder <Michael.Wiedmer@pdg-software.com>
2021-12-10 11:47:18 +0100

    introduce autoboxing

---
+++ src/main/java/ch/finnova/java/schulung/autoboxing/Boxer.java
@@ -0,0 +1,29 @@
+package ch.finnova.java.schulung.autoboxing;
+
+public class Boxer {
+    public static void main(String[] args) {
+        boolean b = false;
+        char c = 'c';
+        short s = 19;
+        int i = 9;
+        long l = 12l;
+        float f = 6.6f;
+        double d = 4.4d;
+
+        Boolean bb = b;
+        Character cc = c;
+        Short ss = s;
+        Integer  ii = i;
+        Long ll = l;
+        Float ff = f;
+        Double dd = d;
+
+        b =bb;
+        c = cc;
+        s = ss;
+        i = ii;
+        l = ll;
+        f = ff;
+        d = dd;
+    }
+}
