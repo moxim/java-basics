cffa7e65134e07478836a0b05cf54f9e0d1e0d2b
Michael Wiemder <Michael.Wiedmer@pdg-software.com>
2021-12-10 11:58:41 +0100

    add main() method

--- src/main/java/ch/finnova/java/schulung/people/Person.java
+++ src/main/java/ch/finnova/java/schulung/people/Person.java
@@ -1,37 +1,8 @@
 package ch.finnova.java.schulung.people;
 
 public class Person {
-    private static String name;
-    private Object coolThing;
-
-    public void setName(String newName) {
-        name = newName; // referencing a static variable from within a non-static method is pretty darn dangerous
-    }
-
-    private String getName() {
-        return name;
-    }
-
-    public void sayHello() {
-        System.out.println(name + " says: Hello");
-    }
-
-    public void sayHelloTo(Person eve) {
-        System.out.println(name + " says: Hello " + eve.getName());
-    }
-
-    public static void doSomethingCool() {
-        coolThing = null; // this is impossible
-    }
-
      public static void main(String[] args) {
-        Person adam = new Person();
-        adam.setName("Adam");
-
-        Person eve = new Person();
-        eve.setName("Eve");
-
-        adam.sayHello();
-        adam.sayHelloTo(eve);
+         // Applikation starten, also Objektinstanzen erstellen
+         // und deren Methoden aufrufen
     }
 }
