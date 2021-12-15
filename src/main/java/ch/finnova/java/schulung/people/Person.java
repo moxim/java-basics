e0a1daf068b78a8b236511b26d4a687609c41acc
Michael Wiemder <Michael.Wiedmer@pdg-software.com>
2021-12-15 11:44:32 +0100

    introduce enumerations

--- src/main/java/ch/finnova/java/schulung/people/Person.java
+++ src/main/java/ch/finnova/java/schulung/people/Person.java
@@ -1,5 +1,44 @@
 package ch.finnova.java.schulung.people;
 
 public class Person {
-    String name;
+    public enum GENDER {
+        DIVERS("d"),
+        FEMALE("f"),
+        MALE("m");
+
+        private String gender;
+
+        GENDER(String s) {
+            gender = s;
+        }
+
+        public String getGenderText() {
+            return gender;
+        }
+    }
+
+    public enum GENDER2 {
+        DIVERS,
+        FEMALE,
+        MALE;
+    }
+
+    public static void main(String[] args) {
+        Person p = new Person();
+
+        System.out.println("--");
+        System.out.println(GENDER.valueOf("MALE"));
+
+        for (GENDER gender : GENDER.values()) {
+            System.out.println(gender + " " + gender.getGenderText());
+        }
+
+        System.out.println("--");
+        System.out.println(GENDER2.valueOf("MALE"));
+
+        for (GENDER2 gender : GENDER2.values()) {
+            System.out.println(gender);
+        }
+    }
+
 }
