3707ee3e132f1e314b9f6d358dc4733eec7f4296
Michael Wiemder <Michael.Wiedmer@pdg-software.com>
2021-12-17 10:59:08 +0100

    remove surplus files

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module ch.finnova.java.schulung {
-    requires java.persistence;
-    exports ch.finnova.java.schulung.temperature;
-    exports ch.finnova.java.schulung.basic;
-    // requires jdk.crypto.ec;
-}
