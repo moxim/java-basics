255d2bc02622014e0d36c8c1ae9eac4ef9e29c39
Michael Wiedmer <Michael.Wiedmer@finnova.com>
2021-12-17 10:15:48 +0100

    Revert "add Test"
    This reverts commit b6919eb5d582050ae365e01cbaf25f6d59c18a1f.

--- src/main/java/ch/finnova/java/schulung/generics/SuperTyp.java
+++ src/main/java/ch/finnova/java/schulung/generics/SuperTyp.java
@@ -5,9 +5,6 @@ import ch.finnova.java.schulung.people.Person;
 public class SuperTyp<T extends Person> {
     private T personTyp;
 
-    public SuperTyp(T type) {
-        personTyp = type;
-    }
     public T getPersonTyp() {
         return personTyp;
     }
--- src/main/java/ch/finnova/java/schulung/people/Handwerker.java
+++ src/main/java/ch/finnova/java/schulung/people/Handwerker.java
@@ -1,14 +1,9 @@
 package ch.finnova.java.schulung.people;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Handwerker extends Person {
     private List<Werkzeug> werkzeuge;
-
-    public Handwerker() {
-        werkzeuge = new ArrayList<>();
-    }
 
     public List<Werkzeug> showTools() {
         return werkzeuge;
