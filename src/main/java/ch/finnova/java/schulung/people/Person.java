11b2b3e5471c8cb2016f1d0262022aebe860aa40
Michael Wiemder <Michael.Wiedmer@pdg-software.com>
2021-12-17 10:47:46 +0100

    remove surplus files

--- src/main/java/ch/finnova/java/schulung/people/Person.java
+++
@@ -1,7 +0,0 @@
-package ch.finnova.java.schulung.people;
-
-import javax.persistence.Table;
-
-@Table(name = "PEOPLE")
-public class Person {
-}
--- src/main/java/ch/finnova/java/schulung/people/Werkzeug.java
+++
@@ -1,9 +0,0 @@
-package ch.finnova.java.schulung.people;
-
-public class Werkzeug {
-    private String name;
-
-    public Werkzeug(String werkzeugName) {
-        name = werkzeugName;
-    }
-}
