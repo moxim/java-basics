99f7609a6025a64caf7c25a07a8fb9aa06e821ff
Michael Wiemder <Michael.Wiedmer@pdg-software.com>
2021-12-17 11:16:05 +0100

    add Programmer

---
+++ src/main/java/ch/finnova/java/schulung/people/Programmer.java
@@ -0,0 +1,10 @@
+package ch.finnova.java.schulung.people;
+
+public class Programmer extends Person { // is-A
+    private int linesOfCode;             // has-A
+
+    public int getLinesOfCode() {
+        return linesOfCode;
+    }
+
+}
