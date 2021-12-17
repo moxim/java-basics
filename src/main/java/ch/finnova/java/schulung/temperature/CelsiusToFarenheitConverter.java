3411397e81723f929edad4002eb92449bccd73a5
Michael Wiedmer <Michael.Wiedmer@finnova.com>
2021-12-17 10:22:02 +0100

    remove surplus files

--- src/main/java/ch/finnova/java/schulung/temperature/CelsiusToFarenheitConverter.java
+++
@@ -1,8 +0,0 @@
-package ch.finnova.java.schulung.temperature;
-
-public class CelsiusToFarenheitConverter extends TemperatureConverter {
-    @Override
-    public float convert(float celsius) {
-        return celsius * 9 / 5 + 32;
-    }
-}
